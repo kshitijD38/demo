65e3b5e17d0d37ed3bf3b29cae91d200f402533b
kshitijD38 <kshitijdhande38@gmail.com>
2022-04-05 23:51:18 +0530

    demo2

--- src/components/ListCallInfo.js
+++ src/components/ListCallInfo.js
@@ -10,7 +10,7 @@ function ListCallInfo(props) {
       headers: { "Content-Type": "application/json" },
     };
     console.log(" req: ", requestOptions);
-    fetch("https://192.168.1.7:9090/list/callInfo", requestOptions)
+    fetch("https://34.214.61.86:5004/list/call/info", requestOptions)
       .then((response) => response.json())
       .then((data) => setRecords(data));
   }, []);
